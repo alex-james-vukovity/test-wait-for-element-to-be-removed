b46449766d9844d9a81f1c3c047166bfdf6327cf
alex-james-vukovity <alex.vukovity@gmail.com>
2021-04-13 19:16:18 +0100

    test: find all example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { memo } from "react"
+import { useQueries, QueryClient, QueryClientProvider } from "react-query"
+import { ReactQueryDevtools } from "react-query/devtools"
+import { ErrorBoundary } from "react-error-boundary"
 
-function App() {
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
+const FallbackComponent = ({ error, resetErrorBoundary }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export const App = () => {
+  return (
+    <ErrorBoundary FallbackComponent={FallbackComponent}>
+      <QueryClientProvider client={queryClient}>
+        <Fetch />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ErrorBoundary>
+  )
+}
+
+const Data = memo(({ data }) => (
+  <section>
+    {data.map((post) => (
+      <p key={post.id}>{post.title}</p>
+    ))}
+  </section>
+))
+
+const Fetch = () => {
+  const queryFn = async ({ queryKey }) => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/${queryKey[0]}`
+    )
+    if (!response.ok) throw new Error("path wrong")
+    return response.json()
+  }
+
+  const [
+    { isLoading: isLoadingPosts, data: posts },
+    { isLoading: isLoadingTodos, data: todos },
+  ] = useQueries([
+    { queryKey: "posts", queryFn },
+    { queryKey: "todos", queryFn },
+  ])
+
+  if (isLoadingPosts || isLoadingTodos) {
+    return (
+      <>
+        <div role="spinbutton">Loading...</div>
+        <div role="spinbutton">Loading...</div>
+        <div role="spinbutton">Loading...</div>
+      </>
+    )
+  }
+
+  return (
+    <main>
+      <h1>Title</h1>
+      <Data data={posts} />
+      <Data data={todos} />
+    </main>
+  )
+}
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,49 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { QueryClientProvider } from "react-query"
+import { rest } from "msw"
+import { setupServer } from "msw/node"
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import { App, queryClient } from "./App"
+
+const response = [
+  {
+    id: 1,
+    title: "...",
+  },
+]
+
+const urlPosts = "https://jsonplaceholder.typicode.com/posts"
+const urlTodos = "https://jsonplaceholder.typicode.com/todos"
+
+const server = setupServer()
+
+beforeAll(() =>
+  server.listen({
+    onUnhandledRequest: "warn",
+  })
+)
+afterEach(() => {
+  queryClient.clear()
+  server.resetHandlers()
+})
+afterAll(() => server.close())
+
+test("should render the heading", async () => {
+  server.use(rest.get(urlPosts, (req, res, ctx) => res(ctx.json(response))))
+  server.use(rest.get(urlTodos, (req, res, ctx) => res(ctx.json(response))))
+
+  render(
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  )
+
+  await waitForElementToBeRemoved(await screen.findByRole("spinbutton"))
+
+  const heading = screen.getByRole("heading", { name: "Title" })
+  expect(heading).toBeInTheDocument()
+})
